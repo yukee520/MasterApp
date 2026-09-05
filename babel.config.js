18fdd5ad4c898204f37f603ed8f8b1a128c0241f
yukee520 <your-email@example.com>
2026-09-05 14:45:33 +0800

    Fix Metro configuration and update build workflow

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-transformer'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,22 @@
 const {getDefaultConfig} = require('@react-native/metro-config');
 
 module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig(__dirname);
-  return defaultConfig;
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('metro-react-native-babel-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
 })();
